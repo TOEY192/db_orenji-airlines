55f14d408c322517ffbc1b020d69ff1070f2493f
phutawan <nightcort2332@gmail.com>
2025-02-26 15:22:02 +0700

    added login and register api

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ connection.connect((err) => {
     console.log("Connected to MySQL!");
 });
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     res.json({msg: "test"})
 })
 
@@ -29,5 +29,71 @@ app.get("/users", (req, res) => {
             return res.status(500).send(err);
         }
         res.json(results);
+    });
+});
+
+// LOGIN API
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    // ตรวจสอบผู้ใช้จากฐานข้อมูล
+    connection.query("SELECT * FROM Users WHERE email = ?", [email], (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        if (results.length === 0) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const user = results[0];
+
+        // ตรวจสอบรหัสผ่านจากการนำค่าที่ป้อนเข้ามาแล้วนำไป hash แล้วค่อยไปเช็คใน database ว่าตรงไหม
+        bcrypt.compare(password, user.password, (err, match) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            if (match) {
+                // รหัสผ่านถูกต้อง
+                res.json({ message: "Login successful", userId: user.id });
+            } else {
+                // รหัสผ่านไม่ถูกต้อง
+                res.status(401).json({ message: "Invalid email or password" });
+            }
+        });
+    });
+});
+
+// REGISTER API
+app.post("/register", (req, res) => {
+    const { username, email, password } = req.body;
+
+    // เข้ารหัสรหัสผ่านและบันทึกลงในฐานข้อมูล
+    // จะทำการเข้ารหัสของรหัสผ่านเพื่อความปลอดภัย (hashedPassword)
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            console.log('error');
+            return res.status(500).send(err);
+        }
+
+        connection.query("INSERT INTO Users (username, email, password) VALUES (?, ?, ?)", [username, email, hashedPassword], (err, results) => {
+            if (err) {
+                connection.query("SELECT COUNT(*) AS user_count FROM Users", (err, results) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    
+                    const userCount = results[0].user_count;
+                    connection.query("ALTER TABLE Users AUTO_INCREMENT = ?", [userCount], (err, results) => {
+                        if (err) {
+                            return res.status(500).send(err);
+                        }
+                    })
+                })
+                return res.status(500).send(err);
+            }
+            res.json({ message: "User registered successfully" });
+        });
     });
 });
