4cbdda9d72b853c61473f0b4ffdb7cf7ff690c97
phutawan <nightcort2332@gmail.com>
2025-03-04 20:56:55 +0700

    fix edit profile

--- js/edit-history.js
+++ js/edit-history.js
@@ -2,23 +2,22 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
         const token = localStorage.getItem('token');
         // เรียก API เพื่อดึงข้อมูลโปรไฟล์ของผู้ใช้
-        const response = await fetch("https://db-orenji-airlines.onrender.com/user-profile", {
+        await fetch("https://db-orenji-airlines.onrender.com/user-profile", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${token}` // ถ้ามีระบบ Auth
             }
-        });
-        
-        if (!response.ok) {
-            throw new Error("Failed to fetch profile data");
-        }
-        
-        const userData = await response.json();
-        console.log(userData)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            document.getElementById('name').innerHTML = userData.firstName + " " + userData.lastName;
+        })
 
         // นำข้อมูลมาใส่ใน placeholder
-        document.getElementById('name').innerHTML = userData.firstName + " " + userData.lastName;
 
     } catch (error) {
         console.error("Error loading profile:", error);
