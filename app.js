0712444bbf5153921a5f27c23124bdadf659184d
phutawan <nightcort2332@gmail.com>
2025-03-04 08:01:49 +0700

    search flight

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ app.post('/show-flight', async (req, res) => {
         const arrivalId = arrivalResult[0].airport_id;
 
         // ค้นหาเที่ยวบิน
-        const flightSql = 'SELECT flight_code FROM Flights WHERE departure_airport_id = ? AND arrival_airport_id = ?';
+        const flightSql = 'SELECT flight_code, departure_time, arrival_time FROM Flights WHERE departure_airport_id = ? AND arrival_airport_id = ?';
         const [flights] = await connection.promise().query(flightSql, [departureId, arrivalId]);
 
         res.json(flights); // ส่งผลลัพธ์กลับไป
