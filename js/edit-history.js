825528b4aaa6be73c3a9c0829e0c2e62278d9bf9
KissMeRight <augdead0123@gmail.com>
2025-03-05 15:30:32 +0700

    update

--- js/edit-history.js
+++ js/edit-history.js
@@ -4,24 +4,24 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         console.log(token)
         // เรียก API เพื่อดึงข้อมูลโปรไฟล์ของผู้ใช้
-        await fetch("https://db-orenji-airlines.onrender.com/user-profile", {
+        const response = await fetch("https://db-orenji-airlines.onrender.com/user-profile", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${token}` // ถ้ามีระบบ Auth
             }
         })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data)
-            console.log(data[0].firstName)
-            console.log(data[0].lastName)
+
+        if(!response.ok) {
+            throw new Error("Failed to fetch user profile");
+        }
+
+        const data = await response.json();
+        if(data.lenght > 0) {
             document.getElementById('name').innerHTML = data[0].firstName + " " + data[0].lastName;
             document.getElementById('email').innerHTML = data[0].email;
             document.getElementById('passportNumber').innerHTML = data[0].passportNumber;
-        })
+        }
 
         // นำข้อมูลมาใส่ใน placeholder
 
