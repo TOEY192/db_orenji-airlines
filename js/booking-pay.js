ede289ce1775deccd64127a5d01d17e8a36f1062
TOEY192 <nightcort2332@gmail.com>
2025-03-05 19:25:09 +0700

    seat

--- js/booking-pay.js
+++ js/booking-pay.js
@@ -4,6 +4,21 @@ document.addEventListener("DOMContentLoaded", async () => {
         const flightCode = urlParams.get('flight_code');
         console.log(flightCode)
         document.getElementById('code').innerHTML = flightCode;
+
+        await fetch('https://db-orenji-airlines.onrender.com/seats', {
+            method: 'GET'
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let li = document.createElement("li");
+            const fclass = document.getElementById('first-class');
+            for(let i = 0; i < 5; i++) {
+                li.textContent = data.seat_number
+                fclass.appendChild(li)
+            }
+        })
     } catch (error) {
         console.error("Error :", error);
     }
